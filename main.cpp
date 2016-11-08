5a80e28a6e4526ae7d98ea6bc4ed103113d39091
Jian Wei <jianwei@jianwei-deb8-64>
2016-11-08 17:28:23 -0500

    first edit

--- main.cpp
+++ main.cpp
@@ -11,6 +11,24 @@ struct Node {
 
 using pNode = std::shared_ptr<Node>;
 
+pNode createList(const std::vector<int> & nums) {
+    pNode head = nullptr;
+    for (auto it = nums.rbegin(); it != nums.rend(); ++it)
+        head = std::make_shared<Node>(*it, head);
+    return head;
+}
+
+void printList(const pNode & node) {
+    if (node != nullptr) {
+        pNode p = node;
+        while (p != nullptr) {
+            std::cout << p->val << " ";
+            p = p->next;
+        }
+        std::cout << std::endl;
+    }
+}
+
 class solution {
   public:
     pNode reverseKGroup(const pNode & head, int k) {
