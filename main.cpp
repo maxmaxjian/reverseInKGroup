784bb7dcec90052222b77b720d9b0b4ee3e9037a
maxmaxjian <jianw812@gmail.com>
2016-11-08 18:29:55 -0800

    first version

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include <vector>
 #include <memory>
+#include <iterator>
+#include <algorithm>
+#include <ctime>
 
 struct Node {
     int val;
@@ -32,10 +36,52 @@ void printList(const pNode & node) {
 class solution {
   public:
     pNode reverseKGroup(const pNode & head, int k) {
-        
+      if (getLength(head) < k) {
+	return head;
+      }
+      else {
+	pNode newhead = head;
+	pNode curr = head;
+	int count = 1;
+	while (count < k) {
+	  auto temp = curr->next;
+	  curr->next = temp->next;
+	  temp->next = newhead;
+	  newhead = temp;
+	  count++;
+	}
+	curr->next = reverseKGroup(curr->next, k);
+	return newhead;
+      }
     }
+
+private:
+  int getLength(const pNode & head) {
+    int len = 0;
+    pNode p = head;
+    while (p != nullptr) {
+      len++;
+      p = p->next;
+    }
+    return len;
+  }
 };
 
 int main() {
-    
+  // srand(time(0));
+  // std::vector<int> nums;
+  // std::generate_n(std::inserter(nums, nums.begin()), 10, [](){
+  //     return int(rand()%20);
+  //   });
+  std::vector<int> nums{1,2,3,4,5};
+  
+  pNode list = createList(nums);
+  std::cout << "Before reversing:\n";
+  printList(list);
+
+  int k = 2;
+  solution soln;
+  pNode rev = soln.reverseKGroup(list, k);
+  std::cout << "After reversing:\n";
+  printList(rev);
 }
